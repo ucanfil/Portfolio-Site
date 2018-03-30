f2573f0ff2e54c4636ad8f8572304d6779f574d0
Burak Tilek <burak.tilek@gmail.com>
2018-03-31 00:15:49 +0300

    Fade-in effect added without  using jQuery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 document.body.className = "fade";
-document.body.addEventListener("DOMContentLoaded", function(e) {
-    document.body.className = "";
-}
+
+/* Below code belongs to Dustin Diaz >> http://dustindiaz.com/smallest-domready-ever  */
+
+function r(f) { /in/.test(document.readyState) ? setTimeout(r, 9, f) : f() }
+r(function () { document.body.className = "";});
